359196e8c3e1fb40a41e7f19e29080b3bc51563d
pchchv <ipchchv@gmail.com>
2022-11-19 20:37:21 +0300

    fix

--- RustBook/guessing_game/src/main.rs
+++ RustBook/guessing_game/src/main.rs
@@ -20,8 +20,8 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue.
-        },
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
