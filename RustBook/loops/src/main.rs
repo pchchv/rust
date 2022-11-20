1305c7567790b26a20e1afe584d2ba03b58e5998
pchchv <ipchchv@gmail.com>
2022-11-20 17:46:21 +0300

    Loops. For. Collections

--- RustBook/loops/src/main.rs
+++ RustBook/loops/src/main.rs
@@ -52,13 +52,21 @@
 //     println!("LIFTOFF!!!");
 // }
 
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     let mut index = 0;
+
+//     while index < 5 {
+//         println!("the value is: {}", a[index]);
+
+//         index += 1;
+//     }
+// }
+
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-
-        index += 1;
+    for element in a {
+        println!("the value is: {element}");
     }
 }
