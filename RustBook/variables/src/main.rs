5b76387a995b3a4ceb55fc38feef5bf53412caef
pchchv <ipchchv@gmail.com>
2022-11-20 13:10:31 +0300

    Variables. Constants

--- RustBook/variables/src/main.rs
+++ RustBook/variables/src/main.rs
@@ -1,6 +1,10 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    println!("Three hours in seconds = {THREE_HOURS_IN_SECONDS}")
 }
