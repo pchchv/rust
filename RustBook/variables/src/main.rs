a0a893b4ca3e4b92151a04d0c4e856a2efafdebc
pchchv <ipchchv@gmail.com>
2022-11-20 13:16:22 +0300

    Variables. Mutability

--- RustBook/variables/src/main.rs
+++ RustBook/variables/src/main.rs
@@ -1,10 +1,31 @@
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+
+//     println!("Three hours in seconds = {THREE_HOURS_IN_SECONDS}")
+// }
 
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
 
-    println!("Three hours in seconds = {THREE_HOURS_IN_SECONDS}")
+    // Ok
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    // Error
+    let mut spaces = "   ";
+    spaces = spaces.len();
 }
