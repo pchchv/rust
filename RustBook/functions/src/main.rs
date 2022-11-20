44439d1dea90292c7eaf4ce3efc04b2552eebf81
pchchv <ipchchv@gmail.com>
2022-11-20 16:54:52 +0300

    Functions. Call

--- RustBook/functions/src/main.rs
+++ RustBook/functions/src/main.rs
@@ -1,9 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
 
-    another_function();
+//     another_function();
+// }
+
+// fn another_function() {
+//     println!("Another function.");
+// }
+
+fn main() {
+    another_function(5);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
