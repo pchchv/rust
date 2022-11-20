be3472f8b443d819c1d6b271273edab848c3a5df
pchchv <ipchchv@gmail.com>
2022-11-20 17:05:34 +0300

    Branches. If. Inequality

--- RustBook/branches/src/main.rs
+++ RustBook/branches/src/main.rs
@@ -1,9 +1,17 @@
+// fn main() {
+//     let number = 7;
+
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number != 0 {
+        println!("number was something other than zero");
     }
 }
