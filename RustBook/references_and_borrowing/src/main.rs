3c67de9c0fd5a368e0e3e734d45a4382feea8409
pchchv <ipchchv@gmail.com>
2022-11-22 21:57:42 +0300

    References. Dangle

--- RustBook/references_and_borrowing/src/main.rs
+++ RustBook/references_and_borrowing/src/main.rs
@@ -30,14 +30,24 @@
 //     some_string.push_str(", world");
 // }
 
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     let r1 = &s; // no problem
+//     let r2 = &s; // no problem
+//     println!("{} and {}", r1, r2);
+//     // variables r1 and r2 will not be used after this point
+
+//     let r3 = &mut s; // no problem
+//     println!("{}", r3);
+// }
+
 fn main() {
-    let mut s = String::from("hello");
+    let reference_to_nothing = dangle();
+}
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
+fn dangle() -> &String {
+    let s = String::from("hello");
 
-    let r3 = &mut s; // no problem
-    println!("{}", r3);
+    &s
 }
