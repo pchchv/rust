19b2b5b73865b02bcae6f0fcedbdca062aad2bc3
pchchv <ipchchv@gmail.com>
2022-11-22 22:19:49 +0300

    Strings/ Static str

--- RustBook/slices/src/main.rs
+++ RustBook/slices/src/main.rs
@@ -10,18 +10,51 @@
 //     s.len()
 // }
 
+// fn main() {
+//     let mut s = String::from("hello world");
+
+//     let word = first_word(&s); // word will get the value 5
+
+//     s.clear(); // this empties the String, making it equal to ""
+
+//     // word still has the value 5 here, but there's no more string that
+//     // we could meaningfully use the value 5 with. word is now totally invalid!
+// }
+
+// fn first_word(s: &String) -> &str {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
+
+//     &s[..]
+// }
+
 fn main() {
-    let mut s = String::from("hello world");
+    let my_string = String::from("hello world");
 
-    let word = first_word(&s); // word will get the value 5
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
 
-    s.clear(); // this empties the String, making it equal to ""
+    let my_string_literal = "hello world";
 
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
