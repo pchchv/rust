856cdc91a9c6a765924fddbef1156a506f7d4559
pchchv <ipchchv@gmail.com>
2022-11-22 21:56:00 +0300

    References. Mutable

--- RustBook/references_and_borrowing/src/main.rs
+++ RustBook/references_and_borrowing/src/main.rs
@@ -20,12 +20,24 @@
 //     some_string.push_str(", world"); // Error!
 // }
 
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     change(&mut s);
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
-}
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
