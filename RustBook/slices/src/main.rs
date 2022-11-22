870d0c481db798518562aa4f1ac173ba349bb561
pchchv <ipchchv@gmail.com>
2022-11-22 22:14:34 +0300

    Slices. Strings

--- RustBook/slices/src/main.rs
+++ RustBook/slices/src/main.rs
@@ -1,14 +1,15 @@
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
 
-    s.len()
-}
+//     s.len()
+// }
+
 fn main() {
     let mut s = String::from("hello world");
 
@@ -18,4 +19,16 @@ fn main() {
 
     // word still has the value 5 here, but there's no more string that
     // we could meaningfully use the value 5 with. word is now totally invalid!
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
