80959b4aaf2a8e6aca1c55dace2ba205486ae56f
pchchv <ipchchv@gmail.com>
2022-11-22 22:42:00 +0300

    Structs. Tuple struct

--- RustBook/structs/src/main.rs
+++ RustBook/structs/src/main.rs
@@ -1,49 +1,57 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
+
+// // fn build_user(email: String, username: String) -> User {
+// //     User {
+// //         active: true,
+// //         username: username,
+// //         email: email,
+// //         sign_in_count: 1,
+// //     }
+// // }
+
+// // fn main() {
+// //     let mut user1 = User {
+// //         active: true,
+// //         username: String::from("someusername123"),
+// //         email: String::from("someone@example.com"),
+// //         sign_in_count: 1,
+// //     };
+
+// //     user1.email = String::from("anotheremail@example.com");
+// // }
 
 // fn build_user(email: String, username: String) -> User {
 //     User {
 //         active: true,
-//         username: username,
-//         email: email,
+//         username,
+//         email,
 //         sign_in_count: 1,
 //     }
 // }
 
 // fn main() {
-//     let mut user1 = User {
-//         active: true,
-//         username: String::from("someusername123"),
-//         email: String::from("someone@example.com"),
-//         sign_in_count: 1,
-//     };
+//     let mut user1 = build_user(
+//         String::from("someone@example.com"),
+//         String::from("someusername123"),
+//     );
 
 //     user1.email = String::from("anotheremail@example.com");
+
+//     let user2 = User {
+//         email: String::from("another@example.com"),
+//         ..user1
+//     };
 // }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let mut user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
-
-    user1.email = String::from("anotheremail@example.com");
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
