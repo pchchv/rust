2f12cf562f69b0f0653ee2724f7df7c462150761
pchchv <ipchchv@gmail.com>
2022-11-23 14:50:44 +0300

    Enums. Match. Templates

--- RustBook/enums/src/main.rs
+++ RustBook/enums/src/main.rs
@@ -1,19 +1,38 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    println!("{}", value_in_cents(Coin::Quarter))
+    println!("{}", value_in_cents(Coin::Quarter(UsState::Alaska)))
 }
