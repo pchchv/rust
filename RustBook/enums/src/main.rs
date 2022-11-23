b6736e9dcfe7bcea7ad6e13e15beccdfd987cb94
pchchv <ipchchv@gmail.com>
2022-11-23 14:48:06 +0300

    Match

--- RustBook/enums/src/main.rs
+++ RustBook/enums/src/main.rs
@@ -4,3 +4,16 @@ enum Coin {
     Dime,
     Quarter,
 }
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    println!("{}", value_in_cents(Coin::Quarter))
+}
