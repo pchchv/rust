a0256badbe975b958d11795121a5a97c7fef28d1
pchchv <ipchchv@gmail.com>
2022-11-23 20:42:15 +0300

    Modules. Path. Super

--- RustBook/restaurant/src/lib.rs
+++ RustBook/restaurant/src/lib.rs
@@ -2,16 +2,27 @@ mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
 
-        // fn seat_at_table() {}
+        fn seat_at_table() {}
     }
 
-    // mod serving {
-    //     fn take_order() {}
+    mod serving {
+        fn take_order() {}
 
-    //     fn serve_order() {}
+        fn serve_order() {}
 
-    //     fn take_payment() {}
-    // }
+        fn take_payment() {}
+    }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
 
 pub fn eat_at_restaurant() {
