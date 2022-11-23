45e9715507d2d66edd1193cf044800850c13e55e
pchchv <ipchchv@gmail.com>
2022-11-23 20:50:47 +0300

    Modules. Enums

--- RustBook/restaurant/src/lib.rs
+++ RustBook/restaurant/src/lib.rs
@@ -14,9 +14,12 @@ mod front_of_house {
     }
 }
 
-fn deliver_order() {}
-
 mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     pub struct Breakfast {
         pub toast: String,
         seasonal_fruit: String,
@@ -40,11 +43,8 @@ mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    // // Absolute path
-    // crate::front_of_house::hosting::add_to_waitlist();
-
-    // // Relative path
-    // front_of_house::hosting::add_to_waitlist();
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
@@ -55,4 +55,7 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
