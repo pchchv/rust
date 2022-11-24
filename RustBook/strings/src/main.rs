c7f5f98c9b59c1cd87d48045680c9d4e782a447a
pchchv <ipchchv@gmail.com>
2022-11-24 20:22:05 +0300

    Strings. Сoncatenation

--- RustBook/strings/src/main.rs
+++ RustBook/strings/src/main.rs
@@ -3,15 +3,27 @@ fn main() {
 
     //     let s = String::from("initial contents");
 
-    let hello = String::from("السلام عليكم");
-    let hello = String::from("Dobrý den");
-    let hello = String::from("Hello");
-    let hello = String::from("שָׁלוֹם");
-    let hello = String::from("नमस्ते");
-    let hello = String::from("こんにちは");
-    let hello = String::from("안녕하세요");
-    let hello = String::from("你好");
-    let hello = String::from("Olá");
-    let hello = String::from("Здравствуйте");
-    let hello = String::from("Hola");
+    // let hello = String::from("السلام عليكم");
+    // let hello = String::from("Dobrý den");
+    // let hello = String::from("Hello");
+    // let hello = String::from("שָׁלוֹם");
+    // let hello = String::from("नमस्ते");
+    // let hello = String::from("こんにちは");
+    // let hello = String::from("안녕하세요");
+    // let hello = String::from("你好");
+    // let hello = String::from("Olá");
+    // let hello = String::from("Здравствуйте");
+    // let hello = String::from("Hola");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
 }
