889bb80c43c432b7ce74fcca564b4429500f475f
pchchv <ipchchv@gmail.com>
2022-11-24 20:05:25 +0300

    Vectors. Iteration and change

--- RustBook/vectors/src/main.rs
+++ RustBook/vectors/src/main.rs
@@ -20,8 +20,13 @@ fn main() {
     //     None => println!("There is no third element."),
     // }
 
-    let v = vec![100, 32, 57];
-    for i in &v {
-        println!("{i}");
+    // let v = vec![100, 32, 57];
+    // for i in &v {
+    //     println!("{i}");
+    // }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
     }
 }
