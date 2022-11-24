3764a223c7a17cf473adeba19d2f93e42e51ff00
pchchv <ipchchv@gmail.com>
2022-11-24 20:16:55 +0300

    Strings. UTF-8

--- RustBook/strings/src/main.rs
+++ RustBook/strings/src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    let mut s = String::new();
+    //     let mut s = String::new();
 
-    let s = String::from("initial contents");
+    //     let s = String::from("initial contents");
+
+    let hello = String::from("السلام عليكم");
+    let hello = String::from("Dobrý den");
+    let hello = String::from("Hello");
+    let hello = String::from("שָׁלוֹם");
+    let hello = String::from("नमस्ते");
+    let hello = String::from("こんにちは");
+    let hello = String::from("안녕하세요");
+    let hello = String::from("你好");
+    let hello = String::from("Olá");
+    let hello = String::from("Здравствуйте");
+    let hello = String::from("Hola");
 }
