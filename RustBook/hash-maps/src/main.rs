45480310bfacebfb8756d3c20f50860b6603e95c
pchchv <ipchchv@gmail.com>
2022-11-24 20:48:10 +0300

    Hash maps. Update

--- RustBook/hash-maps/src/main.rs
+++ RustBook/hash-maps/src/main.rs
@@ -1,27 +1,38 @@
 fn main() {
     use std::collections::HashMap;
 
-    let mut scores = HashMap::new();
+    // let mut scores = HashMap::new();
+
+    // // scores.insert(String::from("Blue"), 10);
+    // // scores.insert(String::from("Yellow"), 50);
+
+    // // // let team_name = String::from("Blue");
+    // // // let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    // // for (key, value) in &scores {
+    // //     println!("{key}: {value}");
+    // // }
+
+    // // scores.insert(String::from("Blue"), 10);
+    // // scores.insert(String::from("Blue"), 25);
+
+    // // println!("{:?}", scores);
 
     // scores.insert(String::from("Blue"), 10);
-    // scores.insert(String::from("Yellow"), 50);
 
-    // // let team_name = String::from("Blue");
-    // // let score = scores.get(&team_name).copied().unwrap_or(0);
-
-    // for (key, value) in &scores {
-    //     println!("{key}: {value}");
-    // }
-
-    // scores.insert(String::from("Blue"), 10);
-    // scores.insert(String::from("Blue"), 25);
+    // scores.entry(String::from("Yellow")).or_insert(50);
+    // scores.entry(String::from("Blue")).or_insert(50);
 
     // println!("{:?}", scores);
 
-    scores.insert(String::from("Blue"), 10);
+    let text = "hello world wonderful world";
 
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
+    let mut map = HashMap::new();
 
-    println!("{:?}", scores);
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
