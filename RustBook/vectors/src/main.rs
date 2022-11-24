b7f130ad0b76a25da74799e1f5ec8dcb6ca36f27
pchchv <ipchchv@gmail.com>
2022-11-24 20:04:25 +0300

    Vectors. Iteration

--- RustBook/vectors/src/main.rs
+++ RustBook/vectors/src/main.rs
@@ -9,14 +9,19 @@ fn main() {
     // v.push(7);
     // v.push(8);
 
-    let v = vec![1, 2, 3, 4, 5];
+    // let v = vec![1, 2, 3, 4, 5];
 
-    let third: &i32 = &v[2];
-    println!("The third element is {third}");
+    // let third: &i32 = &v[2];
+    // println!("The third element is {third}");
 
-    let third: Option<&i32> = v.get(2);
-    match third {
-        Some(third) => println!("The third element is {third}"),
-        None => println!("There is no third element."),
+    // let third: Option<&i32> = v.get(2);
+    // match third {
+    //     Some(third) => println!("The third element is {third}"),
+    //     None => println!("There is no third element."),
+    // }
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
     }
 }
