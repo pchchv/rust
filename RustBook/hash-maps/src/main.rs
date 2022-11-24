e76b66c572bb084962217eb88aea67d1d750b2fa
pchchv <ipchchv@gmail.com>
2022-11-24 20:43:24 +0300

    Hash maps. Update

--- RustBook/hash-maps/src/main.rs
+++ RustBook/hash-maps/src/main.rs
@@ -3,13 +3,18 @@ fn main() {
 
     let mut scores = HashMap::new();
 
+    // scores.insert(String::from("Blue"), 10);
+    // scores.insert(String::from("Yellow"), 50);
+
+    // // let team_name = String::from("Blue");
+    // // let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    // for (key, value) in &scores {
+    //     println!("{key}: {value}");
+    // }
+
     scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    scores.insert(String::from("Blue"), 25);
 
-    // let team_name = String::from("Blue");
-    // let score = scores.get(&team_name).copied().unwrap_or(0);
-
-    for (key, value) in &scores {
-        println!("{key}: {value}");
-    }
+    println!("{:?}", scores);
 }
