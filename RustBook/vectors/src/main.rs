a0c1704c3f791d6527a40d0e667fbd19f63e9e0a
pchchv <ipchchv@gmail.com>
2022-11-24 19:55:36 +0300

    Vectors. Get data

--- RustBook/vectors/src/main.rs
+++ RustBook/vectors/src/main.rs
@@ -1,11 +1,22 @@
 fn main() {
-    let v0: Vec<i32> = Vec::new();
-    let v1 = vec![1, 2, 3];
+    // let v0: Vec<i32> = Vec::new();
+    // let v1 = vec![1, 2, 3];
 
-    let mut v = Vec::new();
+    // let mut v = Vec::new();
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
+    // v.push(5);
+    // v.push(6);
+    // v.push(7);
+    // v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
 }
