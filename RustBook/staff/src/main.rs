7714a9b83c280bd236d51cebf018442a0bcd4b69
pchchv <ipchchv@gmail.com>
2022-11-27 21:36:13 +0300

    Implement a call to the required function

--- RustBook/staff/src/main.rs
+++ RustBook/staff/src/main.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
 use std::io;
 
+fn adder(command: String, staff: &HashMap<String, Vec<&str>>) {}
+
+fn getter(dept: &str) {}
+
 fn main() {
+    let staff: HashMap<String, Vec<&str>> = HashMap::new();
+    let mut command = String::new();
     let mut input = String::new();
 
     println!("To add an employee to the department, type 'Add <Name> to <Department>'");
@@ -10,5 +17,18 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let input = input.split(" ");
+    let mut i = 0;
+
+    for word in input.split(" ") {
+        if i == 0 && (word != "Add" || word != "add") {
+            getter(word)
+        } else if i == 1 || i == 3 {
+            command.push_str(word)
+        }
+        i += 1;
+    }
+
+    if i > 1 {
+        adder(command, &staff)
+    }
 }
