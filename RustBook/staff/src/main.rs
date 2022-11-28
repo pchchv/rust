c877cd63154d545722c13e5ce2777c0f4aee4924
pchchv <ipchchv@gmail.com>
2022-11-28 11:51:09 +0300

    Update main func

--- RustBook/staff/src/main.rs
+++ RustBook/staff/src/main.rs
@@ -32,32 +32,33 @@ fn getter(dept: &str, staff: &HashMap<String, Vec<&str>>) {
 }
 
 fn main() {
-    let mut input = String::new();
-    let mut command: Vec<&str> = Vec::new();
-    let mut staff: HashMap<String, Vec<&str>> = HashMap::new();
-
     println!("To add an employee to the department, type 'Add <Name> to <Department>'");
     println!("To view the employees of a department, enter the name of department");
+    println!("To exit program, type <Exit>");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+    loop {
+        let mut staff: HashMap<String, Vec<&str>> = HashMap::new();
+        let mut command: Vec<&str> = Vec::new();
+        let mut input = String::new();
 
-    let mut i = 0;
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
 
-    for word in input.split(" ") {
-        let word = word.trim();
+        let input = String::from(input.trim());
 
-        if i == 0 && word != "Add" && word != "add" {
-            getter(word, &staff);
-            break;
-        } else if i == 1 || i == 3 {
-            command.push(word)
+        for word in input.split_whitespace() {
+            command.push(word);
         }
-        i += 1;
-    }
 
-    if i == 3 {
-        adder(command[1], vec![command[0]], &mut staff)
+        let first_word = command[0].to_string();
+
+        if first_word == "Add" {
+            adder(command[3], vec![command[1]], &mut staff);
+        } else if first_word == "Exit" {
+            break;
+        } else {
+            getter(command[0], &staff);
+        }
     }
 }
