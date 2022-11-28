8e845fb9ce13fcccce1b93f8d71e50f4f5046f38
pchchv <ipchchv@gmail.com>
2022-11-28 12:16:28 +0300

    Change the process of getting department employees

--- RustBook/staff/src/main.rs
+++ RustBook/staff/src/main.rs
@@ -1,21 +1,6 @@
 use std::collections::HashMap;
 use std::io;
 
-fn getter(dept: &str, staff: &HashMap<String, Vec<&str>>) {
-    let dept_staff = staff.get(dept);
-
-    if dept_staff == Option::None {
-        println!("Department not found!");
-        println!("Maybe the employees of this department have not yet been added");
-    } else {
-        println!("Staff of {dept} department: ");
-
-        for name in dept_staff.unwrap().iter() {
-            println!("    {name}");
-        }
-    }
-}
-
 fn main() {
     println!("To add an employee to the department, type 'Add <Name> to <Department>'");
     println!("To view the employees of a department, enter the name of department");
@@ -33,10 +18,10 @@ fn main() {
         let input = String::from(input.trim());
 
         for word in input.split_whitespace() {
-            command.push(word);
+            command.push(word.trim());
         }
 
-        let first_word = command[0].to_string();
+        let first_word = command[0].trim().to_string();
 
         if first_word == "Add" {
             let name = String::from(command[1]);
@@ -47,7 +32,12 @@ fn main() {
         } else if first_word == "Exit" {
             break;
         } else {
-            getter(command[0], &staff);
+            let values = staff.get_mut(&first_word).expect("No such department");
+            values.sort();
+            println!("{}:", first_word);
+            for value in values {
+                println!("\t{}", value);
+            }
         }
     }
 }
