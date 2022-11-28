467d83be768d13655b7fd4e577a5e97b422fbb08
pchchv <ipchchv@gmail.com>
2022-11-28 12:25:00 +0300

    Refactoring

--- RustBook/staff/src/main.rs
+++ RustBook/staff/src/main.rs
@@ -2,12 +2,13 @@ use std::collections::HashMap;
 use std::io;
 
 fn main() {
+    let mut staff: HashMap<String, Vec<&str>> = HashMap::new();
+
     println!("To add an employee to the department, type 'Add <Name> to <Department>'");
     println!("To view the employees of a department, enter the name of department");
     println!("To exit program, type <Exit>");
 
     loop {
-        let mut staff: HashMap<String, Vec<&str>> = HashMap::new();
         let mut command: Vec<&str> = Vec::new();
         let mut input = String::new();
 
@@ -16,19 +17,18 @@ fn main() {
             .expect("Failed to read line");
 
         let input = String::from(input.trim());
+        let input = input.split_whitespace();
 
-        for word in input.split_whitespace() {
+        for word in input {
             command.push(word.trim());
         }
 
         let first_word = command[0].trim().to_string();
 
         if first_word == "Add" {
-            let name = String::from(command[1]);
-            let department = String::from(command[3]);
-            let department = staff.entry(department).or_insert(vec![]);
+            let department = staff.entry(command[3].to_string()).or_insert(vec![]);
 
-            department.push(&name);
+            department.push(command[1]);
         } else if first_word == "Exit" {
             break;
         } else {
