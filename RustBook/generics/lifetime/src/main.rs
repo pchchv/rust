f7c1069d8c8430cf69951fb7a0bd8beafa3bdd3c
pchchv <ipchchv@gmail.com>
2022-11-29 23:41:39 +0300

    Lifetime

--- RustBook/generics/lifetime/src/main.rs
+++ RustBook/generics/lifetime/src/main.rs
@@ -5,10 +5,20 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         y
     }
 }
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+// fn main() {
+//     let string1 = String::from("abcd");
+//     let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+//     let result = longest(string1.as_str(), string2);
+//     println!("The longest string is {}", result);
+// }
+
+fn main() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
