777ef2f17be283d6692555d17725082127d4eb45
pchchv <ipchchv@gmail.com>
2022-11-29 22:47:49 +0300

    Generics. Structs

--- RustBook/generics/src/main.rs
+++ RustBook/generics/src/main.rs
@@ -34,26 +34,36 @@
 //     println!("The largest char is {}", result);
 // }
 
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+//     let mut largest = &list[0];
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
 
-    largest
+//     largest
+// }
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     let result = largest(&number_list);
+//     println!("The largest number is {}", result);
+
+//     let char_list = vec!['y', 'm', 'a', 'q'];
+
+//     let result = largest(&char_list);
+//     println!("The largest char is {}", result);
+// }
+
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }
