d093dbc6a4a975d25bc8e3c94f157f740e228df8
pchchv <ipchchv@gmail.com>
2022-11-29 22:49:22 +0300

    Generics. Structs. Two types

--- RustBook/generics/src/main.rs
+++ RustBook/generics/src/main.rs
@@ -58,12 +58,23 @@
 //     println!("The largest char is {}", result);
 // }
 
-struct Point<T> {
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+// }
+
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
 }
