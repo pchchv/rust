bb70fd3984f1017cd516f2bf9da25a8039e57a5e
pchchv <ipchchv@gmail.com>
2022-11-29 10:35:44 +0300

    Result. Unwrap and expect

--- RustBook/recoverable_errors/src/main.rs
+++ RustBook/recoverable_errors/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
+// use std::io::ErrorKind;
 
 // fn main() {
 //     let greeting_file_result = File::open("hello.txt");
@@ -27,14 +27,21 @@ use std::io::ErrorKind;
 //     };
 // }
 
+// fn main() {
+//     let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {:?}", error);
+//             })
+//         } else {
+//             panic!("Problem opening the file: {:?}", error);
+//         }
+//     });
+// }
+
 fn main() {
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Problem creating the file: {:?}", error);
-            })
-        } else {
-            panic!("Problem opening the file: {:?}", error);
-        }
-    });
+    let greeting_file = File::open("hello.txt").unwrap();
+
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
 }
