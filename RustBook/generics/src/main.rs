c2de29b4092970ea44cf32806b16e4c46f97cd21
pchchv <ipchchv@gmail.com>
2022-11-29 22:51:49 +0300

    Generics. Enums

--- RustBook/generics/src/main.rs
+++ RustBook/generics/src/main.rs
@@ -68,13 +68,23 @@
 //     let float = Point { x: 1.0, y: 4.0 };
 // }
 
-struct Point<T, U> {
-    x: T,
-    y: U,
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+
+// fn main() {
+//     let both_integer = Point { x: 5, y: 10 };
+//     let both_float = Point { x: 1.0, y: 4.0 };
+//     let integer_and_float = Point { x: 5, y: 4.0 };
+// }
+
+enum Option<T> {
+    Some(T),
+    None,
 }
 
-fn main() {
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
