a56b4f7f331dea2487202b0329a48a6a634de4c9
pchchv <ipchchv@gmail.com>
2022-11-29 10:50:52 +0300

    Result. Operator `?`

--- RustBook/recoverable_errors/src/main.rs
+++ RustBook/recoverable_errors/src/main.rs
@@ -63,10 +63,18 @@ use std::io::{self, Read};
 //     }
 // }
 
+// fn read_username_from_file() -> Result<String, io::Error> {
+//     let mut username_file = File::open("hello.txt")?;
+//     let mut username = String::new();
+//     username_file.read_to_string(&mut username)?;
+//     Ok(username)
+// }
+
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
     let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
     Ok(username)
 }
 
