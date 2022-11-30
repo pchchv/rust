2858b4f1070639d23025ab3b41ce1fd12a00fb60
pchchv <ipchchv@gmail.com>
2022-11-30 11:58:52 +0300

    Tests. Result

--- RustBook/adder/src/lib.rs
+++ RustBook/adder/src/lib.rs
@@ -93,56 +93,72 @@
 //     }
 // }
 
-pub struct Guess {
-    value: i32,
-}
+// pub struct Guess {
+//     value: i32,
+// }
+
+// // impl Guess {
+// //     pub fn new(value: i32) -> Guess {
+// //         if value < 1 || value > 100 {
+// //             panic!("Guess value must be between 1 and 100, got {}.", value);
+// //         }
+
+// //         Guess { value }
+// //     }
+// // }
 
 // impl Guess {
 //     pub fn new(value: i32) -> Guess {
-//         if value < 1 || value > 100 {
-//             panic!("Guess value must be between 1 and 100, got {}.", value);
+//         if value < 1 {
+//             panic!(
+//                 "Guess value must be greater than or equal to 1, got {}.",
+//                 value
+//             );
+//         } else if value > 100 {
+//             panic!(
+//                 "Guess value must be less than or equal to 100, got {}.",
+//                 value
+//             );
 //         }
 
 //         Guess { value }
 //     }
 // }
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            );
-        }
+// // #[cfg(test)]
+// // mod tests {
+// //     use super::*;
 
-        Guess { value }
-    }
-}
+// //     #[test]
+// //     #[should_panic]
+// //     fn greater_than_100() {
+// //         Guess::new(200);
+// //     }
+// // }
 
 // #[cfg(test)]
 // mod tests {
 //     use super::*;
 
 //     #[test]
-//     #[should_panic]
+//     #[should_panic(expected = "less than or equal to 100")]
 //     fn greater_than_100() {
 //         Guess::new(200);
 //     }
 // }
 
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    #[should_panic(expected = "less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
