9b8222306d2b5fbecfa972c5384ea378111dd35f
pchchv <ipchchv@gmail.com>
2022-11-30 11:51:59 +0300

    Tests. Error messages

--- RustBook/adder/src/lib.rs
+++ RustBook/adder/src/lib.rs
@@ -60,8 +60,22 @@
 //     }
 // }
 
-pub fn add_two(a: i32) -> i32 {
-    a + 2
+// pub fn add_two(a: i32) -> i32 {
+//     a + 2
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_adds_two() {
+//         assert_eq!(4, add_two(2));
+//     }
+// }
+
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")
 }
 
 #[cfg(test)]
@@ -69,7 +83,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_adds_two() {
-        assert_eq!(4, add_two(2));
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
