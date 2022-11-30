7e010754b4aac5f02a4f0f053f946d63e2068d18
pchchv <ipchchv@gmail.com>
2022-11-30 11:56:41 +0300

    Tests. should_panic. Expected

--- RustBook/adder/src/lib.rs
+++ RustBook/adder/src/lib.rs
@@ -97,22 +97,51 @@ pub struct Guess {
     value: i32,
 }
 
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 || value > 100 {
+//             panic!("Guess value must be between 1 and 100, got {}.", value);
+//         }
+
+//         Guess { value }
+//     }
+// }
+
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
         }
 
         Guess { value }
     }
 }
 
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     #[should_panic]
+//     fn greater_than_100() {
+//         Guess::new(200);
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
