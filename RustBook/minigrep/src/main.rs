7c2d97a37ab694365f1d7c5c56b58350ed47c821
pchchv <ipchchv@gmail.com>
2022-12-01 22:00:43 +0300

    Refactoring. Error handling

--- RustBook/minigrep/src/main.rs
+++ RustBook/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -20,11 +21,12 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 fn main() {
@@ -38,5 +40,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
