c74bebd0d2d0ec85efba4b091c52058a17cfa8e3
pchchv <ipchchv@gmail.com>
2022-12-01 21:30:23 +0300

    Refactoring. Separate parsing arguments

--- RustBook/minigrep/src/main.rs
+++ RustBook/minigrep/src/main.rs
@@ -1,11 +1,17 @@
 use std::env;
 use std::fs;
 
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
