4c423d2a9fbfd0fcb348cfa21f38546c71f8d527
pchchv <ipchchv@gmail.com>
2022-12-01 20:56:55 +0300

    Integration tests

--- RustBook/adder/src/lib.rs
+++ RustBook/adder/src/lib.rs
@@ -60,19 +60,19 @@
 //     }
 // }
 
-// pub fn add_two(a: i32) -> i32 {
-//     a + 2
-// }
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn it_adds_two() {
-//         assert_eq!(4, add_two(2));
-//     }
-// }
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
 
 // pub fn greeting(name: &str) -> String {
 //     String::from("Hello!")
@@ -147,18 +147,18 @@
 //     }
 // }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() -> Result<(), String> {
-        if 2 + 2 == 4 {
-            Ok(())
-        } else {
-            Err(String::from("two plus two does not equal four"))
-        }
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() -> Result<(), String> {
+//         if 2 + 2 == 4 {
+//             Ok(())
+//         } else {
+//             Err(String::from("two plus two does not equal four"))
+//         }
+//     }
+// }
