0ec8d328c69de4a45756a8d984423cc743eb8c98
pchchv <ipchchv@gmail.com>
2022-12-02 19:38:47 +0300

    Iterators. Consuming adapters

--- RustBook/iterators/src/lib.rs
+++ RustBook/iterators/src/lib.rs
@@ -1,15 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// #[test]
+// fn iterator_demonstration() {
+//     let v1 = vec![1, 2, 3];
+
+//     let mut v1_iter = v1.iter();
+
+//     assert_eq!(v1_iter.next(), Some(&1));
+//     assert_eq!(v1_iter.next(), Some(&2));
+//     assert_eq!(v1_iter.next(), Some(&3));
+//     assert_eq!(v1_iter.next(), None);
+// }
 
 #[test]
-fn iterator_demonstration() {
+fn iterator_sum() {
     let v1 = vec![1, 2, 3];
 
-    let mut v1_iter = v1.iter();
+    let v1_iter = v1.iter();
 
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
 }
