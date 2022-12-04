d7ebb066464cae7628fcc3cf1bbd6ad808bf6295
pchchv <ipchchv@gmail.com>
2022-12-04 22:34:18 +0300

    Creating branch in internal scope

--- RustBook/reference_cycles/src/main.rs
+++ RustBook/reference_cycles/src/main.rs
@@ -51,6 +51,26 @@ struct Node {
     children: RefCell<Vec<Rc<Node>>>,
 }
 
+// fn main() {
+//     let leaf = Rc::new(Node {
+//         value: 3,
+//         parent: RefCell::new(Weak::new()),
+//         children: RefCell::new(vec![]),
+//     });
+
+//     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+//     let branch = Rc::new(Node {
+//         value: 5,
+//         parent: RefCell::new(Weak::new()),
+//         children: RefCell::new(vec![Rc::clone(&leaf)]),
+//     });
+
+//     *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+//     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+// }
+
 fn main() {
     let leaf = Rc::new(Node {
         value: 3,
@@ -58,15 +78,38 @@ fn main() {
         children: RefCell::new(vec![]),
     });
 
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-
-    let branch = Rc::new(Node {
-        value: 5,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
-
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
