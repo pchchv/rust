72eb693b93e9a0e823bb813550aa32279e3f1e67
pchchv <ipchchv@gmail.com>
2022-12-04 20:50:45 +0300

    Box as link

--- RustBook/pointer_box/pointer_deref/src/main.rs
+++ RustBook/pointer_box/pointer_deref/src/main.rs
@@ -1,6 +1,14 @@
+// fn main() {
+//     let x = 5;
+//     let y = &x;
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
+
 fn main() {
     let x = 5;
-    let y = &x;
+    let y = Box::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
