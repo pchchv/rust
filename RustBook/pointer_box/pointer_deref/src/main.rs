63d2ea43f39a82b3fb335910d5be4b93cbed554d
pchchv <ipchchv@gmail.com>
2022-12-04 20:56:52 +0300

    Implementation of the Deref type

--- RustBook/pointer_box/pointer_deref/src/main.rs
+++ RustBook/pointer_box/pointer_deref/src/main.rs
@@ -14,11 +14,21 @@
 //     assert_eq!(5, *y);
 // }
 
+use std::ops::Deref;
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
