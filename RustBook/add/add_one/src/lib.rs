093e8684138d3d738d8e48d65cf0026a17a51761
pchchv <ipchchv@gmail.com>
2022-12-04 19:54:15 +0300

    Implement add_one func

--- RustBook/add/add_one/src/lib.rs
+++ RustBook/add/add_one/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
 
 #[cfg(test)]
@@ -8,7 +8,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = add_one(2);
+        assert_eq!(result, 3);
     }
 }
