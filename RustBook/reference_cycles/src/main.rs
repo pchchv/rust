2cf6b463eede3fbe0d0ed2db8c3e4fe418ea5e17
pchchv <ipchchv@gmail.com>
2022-12-04 22:30:21 +0300

    Creating nodes

--- RustBook/reference_cycles/src/main.rs
+++ RustBook/reference_cycles/src/main.rs
@@ -49,3 +49,15 @@ struct Node {
     value: i32,
     children: RefCell<Vec<Rc<Node>>>,
 }
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+}
