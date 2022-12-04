1a0e3e272f7cf4383146c3e53f7230868946bf99
pchchv <ipchchv@gmail.com>
2022-12-04 21:16:58 +0300

    Forced deletion of a value from memory

--- RustBook/pointer_drop/src/main.rs
+++ RustBook/pointer_drop/src/main.rs
@@ -8,12 +8,21 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+// fn main() {
+//     let c = CustomSmartPointer {
+//         data: String::from("my stuff"),
+//     };
+//     let d = CustomSmartPointer {
+//         data: String::from("other stuff"),
+//     };
+//     println!("CustomSmartPointers created.");
+// }
+
 fn main() {
     let c = CustomSmartPointer {
-        data: String::from("my stuff"),
+        data: String::from("some data"),
     };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointers created.");
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
