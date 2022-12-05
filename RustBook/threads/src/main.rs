8d60bd9fbe85e3d9cbd64ace2e2c60de575ebc50
pchchv <ipchchv@gmail.com>
2022-12-05 12:41:56 +0300

    Waiting for all threads to complete using join

--- RustBook/threads/src/main.rs
+++ RustBook/threads/src/main.rs
@@ -1,8 +1,22 @@
 use std::thread;
 use std::time::Duration;
 
+// fn main() {
+//     thread::spawn(|| {
+//         for i in 1..10 {
+//             println!("hi number {} from the spawned thread!", i);
+//             thread::sleep(Duration::from_millis(1));
+//         }
+//     });
+
+//     for i in 1..5 {
+//         println!("hi number {} from the main thread!", i);
+//         thread::sleep(Duration::from_millis(1));
+//     }
+// }
+
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -13,4 +27,6 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
