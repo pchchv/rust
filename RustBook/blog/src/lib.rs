13f993a2802f64ab2926bf965e5764b47979efb0
pchchv <ipchchv@gmail.com>
2022-12-10 15:27:05 +0300

    A Post with a content method and a DraftPost without a content method

--- RustBook/blog/src/lib.rs
+++ RustBook/blog/src/lib.rs
@@ -1,34 +1,54 @@
 pub struct Post {
-    state: Option<Box<dyn State>>,
+    // state: Option<Box<dyn State>>,
+    content: String,
+}
+
+pub struct DraftPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
+    // pub fn new() -> Post {
+    //     Post {
+    //         state: Some(Box::new(Draft {})),
+    //         content: String::new(),
+    //     }
+    // }
+
+    // pub fn add_text(&mut self, text: &str) {
+    //     self.content.push_str(text);
+    // }
+
+    // pub fn content(&self) -> &str {
+    //     self.state.as_ref().unwrap().content(self)
+    // }
+
+    // pub fn request_review(&mut self) {
+    //     if let Some(s) = self.state.take() {
+    //         self.state = Some(s.request_review())
+    //     }
+    // }
+
+    // pub fn approve(&mut self) {
+    //     if let Some(s) = self.state.take() {
+    //         self.state = Some(s.approve())
+    //     }
+    // }
+
+    pub fn new() -> DraftPost {
+        DraftPost {
             content: String::new(),
         }
     }
 
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
-    }
-
-    pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(self)
-    }
-
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
-        }
     }
 }
 
