defdc13360549592dfcade93aade98ec302262e0
pchchv <ipchchv@gmail.com>
2022-12-10 17:05:42 +0300

    Using unsafe code in the implementation of the split_at_mut function

--- RustBook/unsafe_rust/src/main.rs
+++ RustBook/unsafe_rust/src/main.rs
@@ -1,3 +1,19 @@
+use std::slice;
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
+
 fn main() {
     // let mut num = 5;
 
@@ -22,7 +38,7 @@ fn main() {
 
     let r = &mut v[..];
 
-    let (a, b) = r.split_at_mut(3);
+    let (a, b) = split_at_mut(r, 3);
 
     assert_eq!(a, &mut [1, 2, 3]);
     assert_eq!(b, &mut [4, 5, 6]);
