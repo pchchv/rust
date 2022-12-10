04e0132a2bde0d36b6eaf1aec89d2ea0ea1b58ce
pchchv <ipchchv@gmail.com>
2022-12-10 17:09:29 +0300

    Declaring and calling an extern function defined in another language

--- RustBook/unsafe_rust/src/main.rs
+++ RustBook/unsafe_rust/src/main.rs
@@ -1,45 +1,55 @@
-use std::slice;
+// use std::slice;
 
-fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
-    let len = values.len();
-    let ptr = values.as_mut_ptr();
+// fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+//     let len = values.len();
+//     let ptr = values.as_mut_ptr();
 
-    assert!(mid <= len);
+//     assert!(mid <= len);
 
-    unsafe {
-        (
-            slice::from_raw_parts_mut(ptr, mid),
-            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
-        )
-    }
-}
+//     unsafe {
+//         (
+//             slice::from_raw_parts_mut(ptr, mid),
+//             slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+//         )
+//     }
+// }
 
 fn main() {
-    // let mut num = 5;
+    // // let mut num = 5;
 
-    // let r1 = &num as *const i32;
-    // let r2 = &mut num as *mut i32;
+    // // let r1 = &num as *const i32;
+    // // let r2 = &mut num as *mut i32;
 
-    // let address = 0x012345usize;
-    // let r = address as *const i32;
+    // // let address = 0x012345usize;
+    // // let r = address as *const i32;
 
-    // unsafe {
-    //     println!("r1 is: {}", *r1);
-    //     println!("r2 is: {}", *r2);
-    // }
+    // // unsafe {
+    // //     println!("r1 is: {}", *r1);
+    // //     println!("r2 is: {}", *r2);
+    // // }
 
-    // unsafe fn dangerous() {}
+    // // unsafe fn dangerous() {}
 
-    // unsafe {
-    //     dangerous();
-    // }
+    // // unsafe {
+    // //     dangerous();
+    // // }
 
-    let mut v = vec![1, 2, 3, 4, 5, 6];
+    // let mut v = vec![1, 2, 3, 4, 5, 6];
 
-    let r = &mut v[..];
+    // let r = &mut v[..];
 
-    let (a, b) = split_at_mut(r, 3);
+    // let (a, b) = split_at_mut(r, 3);
 
-    assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
+    // assert_eq!(a, &mut [1, 2, 3]);
+    // assert_eq!(b, &mut [4, 5, 6]);
+
+    extern "C" {
+        fn abs(input: i32) -> i32;
+    }
+
+    fn main() {
+        unsafe {
+            println!("Absolute value of -3 according to C: {}", abs(-3));
+        }
+    }
 }
