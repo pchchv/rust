286475768e8fde2774eeee324019b6b2bde1d690
pchchv <ipchchv@gmail.com>
2022-12-12 00:20:57 +0300

    Modifying ThreadPool to store the sender of a channel that transmits Job instances

--- RustBook/hello/src/lib.rs
+++ RustBook/hello/src/lib.rs
@@ -1,8 +1,11 @@
-use std::thread;
+use std::{sync::mpsc, thread};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -15,13 +18,15 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             workers.push(Worker::new(id));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
