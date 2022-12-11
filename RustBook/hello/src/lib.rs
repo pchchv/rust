acb5342d869ffe02c3b197148d6ad26c40540de0
pchchv <ipchchv@gmail.com>
2022-12-12 00:56:09 +0300

    Explicitly break out of the loop when recv returns an error

--- RustBook/hello/src/lib.rs
+++ RustBook/hello/src/lib.rs
@@ -52,11 +52,19 @@ struct Worker {
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || {
-            while let Ok(job) = receiver.lock().unwrap().recv() {
-                println!("Worker {id} got a job; executing.");
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv();
 
-                job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
             }
         });
 
