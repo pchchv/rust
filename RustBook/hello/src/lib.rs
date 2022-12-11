f72f5977bc9cb75f0f5de373b3812d1fc5c171c4
pchchv <ipchchv@gmail.com>
2022-12-12 00:49:55 +0300

    Joining each thread when the thread pool goes out of scope

--- RustBook/hello/src/lib.rs
+++ RustBook/hello/src/lib.rs
@@ -44,7 +44,7 @@ impl ThreadPool {
 }
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -57,6 +57,21 @@ impl Worker {
             }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
