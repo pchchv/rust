68c2a93026fb0b96d808217b6a5fad3c61ba54f9
pchchv <ipchchv@gmail.com>
2022-12-11 15:59:00 +0300

    A trait with an associated function and a type with an associated function of the same name that also implements the trait

--- RustBook/advanced_traits/src/main.rs
+++ RustBook/advanced_traits/src/main.rs
@@ -36,38 +36,61 @@
 //     }
 // }
 
-trait Pilot {
-    fn fly(&self);
+// trait Pilot {
+//     fn fly(&self);
+// }
+
+// trait Wizard {
+//     fn fly(&self);
+// }
+
+// struct Human;
+
+// impl Pilot for Human {
+//     fn fly(&self) {
+//         println!("This is your captain speaking.");
+//     }
+// }
+
+// impl Wizard for Human {
+//     fn fly(&self) {
+//         println!("Up!");
+//     }
+// }
+
+// impl Human {
+//     fn fly(&self) {
+//         println!("*waving arms furiously*");
+//     }
+// }
+
+// fn main() {
+//     let person = Human;
+
+//     Pilot::fly(&person);
+//     Wizard::fly(&person);
+//     person.fly();
+// }
+
+trait Animal {
+    fn baby_name() -> String;
 }
 
-trait Wizard {
-    fn fly(&self);
-}
+struct Dog;
 
-struct Human;
-
-impl Pilot for Human {
-    fn fly(&self) {
-        println!("This is your captain speaking.");
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
     }
 }
 
-impl Wizard for Human {
-    fn fly(&self) {
-        println!("Up!");
-    }
-}
-
-impl Human {
-    fn fly(&self) {
-        println!("*waving arms furiously*");
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
 
 fn main() {
-    let person = Human;
-
-    Pilot::fly(&person);
-    Wizard::fly(&person);
-    person.fly();
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+    println!("A baby dog is called a {}", Dog::baby_name());
 }
