51b9190f5e18196fbe64fe88aa8c622853e5ce06
pchchv <ipchchv@gmail.com>
2022-12-11 18:25:02 +0300

    Implementing the HelloMacro trait using the parsed Rust code

--- RustBook/hello_macro/hello_macro_derive/src/lib.rs
+++ RustBook/hello_macro/hello_macro_derive/src/lib.rs
@@ -11,3 +11,15 @@ pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
     // Build the trait implementation
     impl_hello_macro(&ast)
 }
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
