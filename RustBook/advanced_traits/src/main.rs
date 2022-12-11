ec6d92161949ff004c2114fd61dfa5b6b5a6ce8b
pchchv <ipchchv@gmail.com>
2022-12-11 15:52:32 +0300

    Two traits are defined to have a fly method and are implemented on the Human type, and a fly method is implemented on Human directly

--- RustBook/advanced_traits/src/main.rs
+++ RustBook/advanced_traits/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+// use std::ops::Add;
 
 // #[derive(Debug, Copy, Clone, PartialEq)]
 // struct Point {
@@ -24,14 +24,48 @@ use std::ops::Add;
 //     );
 // }
 
-struct Millimeters(u32);
+// struct Millimeters(u32);
 
-struct Meters(u32);
+// struct Meters(u32);
 
-impl Add<Meters> for Millimeters {
-    type Output = Millimeters;
+// impl Add<Meters> for Millimeters {
+//     type Output = Millimeters;
 
-    fn add(self, other: Meters) -> Millimeters {
-        Millimeters(self.0 + (other.0 * 1000))
+//     fn add(self, other: Meters) -> Millimeters {
+//         Millimeters(self.0 + (other.0 * 1000))
+//     }
+// }
+
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
     }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+fn main() {
+    let person = Human;
+
+    person.fly();
 }
