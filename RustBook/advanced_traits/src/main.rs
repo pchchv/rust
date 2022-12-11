8f38dec5ac7b8717265ac04f6e76213aaa927e6e
pchchv <ipchchv@gmail.com>
2022-12-11 16:11:39 +0300

    Creating a Wrapper type around Vec<String> to implement Display

--- RustBook/advanced_traits/src/main.rs
+++ RustBook/advanced_traits/src/main.rs
@@ -97,28 +97,42 @@ use std::fmt;
 //     println!("A baby dog is called a {}", Dog::baby_name());
 // }
 
-trait OutlinePrint: fmt::Display {
-    fn outline_print(&self) {
-        let output = self.to_string();
-        let len = output.len();
+// trait OutlinePrint: fmt::Display {
+//     fn outline_print(&self) {
+//         let output = self.to_string();
+//         let len = output.len();
 
-        println!("{}", "*".repeat(len + 4));
-        println!("*{}*", " ".repeat(len + 2));
-        println!("* {} *", output);
-        println!("*{}*", " ".repeat(len + 2));
-        println!("{}", "*".repeat(len + 4));
+//         println!("{}", "*".repeat(len + 4));
+//         println!("*{}*", " ".repeat(len + 2));
+//         println!("* {} *", output);
+//         println!("*{}*", " ".repeat(len + 2));
+//         println!("{}", "*".repeat(len + 4));
+//     }
+// }
+
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+
+// impl fmt::Display for Point {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "({}, {})", self.x, self.y)
+//     }
+// }
+
+// impl OutlinePrint for Point {}
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
     }
 }
 
-struct Point {
-    x: i32,
-    y: i32,
-}
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
 
-impl fmt::Display for Point {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
-    }
+    println!("w = {}", w);
 }
-
-impl OutlinePrint for Point {}
