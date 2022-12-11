0f056c42faefe18e120969d2ba42a591682e8e73
pchchv <ipchchv@gmail.com>
2022-12-11 16:03:14 +0300

    Implementing the OutlinePrint trait on Point

--- RustBook/advanced_traits/src/main.rs
+++ RustBook/advanced_traits/src/main.rs
@@ -109,3 +109,16 @@ trait OutlinePrint: fmt::Display {
         println!("{}", "*".repeat(len + 4));
     }
 }
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl OutlinePrint for Point {}
